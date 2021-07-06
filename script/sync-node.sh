afd8919bf0864ee4c99178845846cde07a58c956
Rob Thijssen <rthijssen@gmail.com>
2021-07-06 10:06:24 +0300

    wip: node sync script

--- script/sync-node.sh
+++ script/sync-node.sh
@@ -7,10 +7,10 @@ temp_dir=$(mktemp -d)
 
 instance_region=$(curl -sL http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
 instance_id=$(curl -sL http://169.254.169.254/latest/meta-data/instance-id)
-instance_name=$(aws ec2 describe-instances --region ${instance_region} --instance-id ${instance_id} --query "Reservations[*].Instances[*].Tags[?Key=='Name'].Value" --output text)
-instance_domain=$(aws ec2 describe-instances --region ${instance_region} --instance-id ${instance_id} --query "Reservations[*].Instances[*].Tags[?Key=='Domain'].Value" --output text)
-instance_cname=$(aws ec2 describe-instances --region ${instance_region} --instance-id ${instance_id} --query "Reservations[*].Instances[*].Tags[?Key=='cname'].Value" --output text)
-instance_source=$(aws ec2 describe-instances --region ${instance_region} --instance-id ${instance_id} --query "Reservations[*].Instances[*].Tags[?Key=='Source'].Value" --output text)
+instance_name=$(aws ec2 describe-tags --region ${instance_region} --filters "Name=resource-id,Values=${instance_id}" "Name=key,Values=Name" --query "Tags[*].Value" --output text)
+instance_domain=$(aws ec2 describe-tags --region ${instance_region} --filters "Name=resource-id,Values=${instance_id}" "Name=key,Values=Domain" --query "Tags[*].Value" --output text)
+instance_cname=$(aws ec2 describe-tags --region ${instance_region} --filters "Name=resource-id,Values=${instance_id}" "Name=key,Values=cname" --query "Tags[*].Value" --output text)
+instance_source=$(aws ec2 describe-tags --region ${instance_region} --filters "Name=resource-id,Values=${instance_id}" "Name=key,Values=Source" --query "Tags[*].Value" --output text)
 config_url=${instance_source/github.com/raw.githubusercontent.com}/main/config/hosts.json
 
 curl -sL ${config_url} | jq --arg hostname ${instance_name} --arg domain ${instance_domain} --arg cname ${instance_cname} --arg region ${instance_region} '.[] | select( .hostname == $hostname and .domain == $domain and .cname == $cname and .region == $region )' > ${temp_dir}/host-config.json
